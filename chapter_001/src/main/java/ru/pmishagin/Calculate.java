49ebcdb2899f7953779e71b7279397ad1feba7d3
Pavel71 <raz_3@mail.ru>
2017-10-10 13:55:17 +0300

    Chaptr001

---
+++ chapter_001/src/main/java/ru/pmishagin/Calculate.java
@@ -0,0 +1,18 @@
+
+/**
+ * Package for calculate task.
+ *
+ * @author Pavel Mishagin (mailto:raz_3@mail.ru)
+ * @version 1.0
+ * @since 06.10.2017
+ */
+
+package ru.pmishagin;
+
+public class Calculate {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
