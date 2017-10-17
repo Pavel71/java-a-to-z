182728d1025c2bb7da67af1a5128e3bb49951fcc
Pavel71 <raz_3@mail.ru>
2017-10-17 10:10:48 +0300

    Max

---
+++ chapter_001/src/main/java/ru/pmishagin/max/Max.java
@@ -0,0 +1,14 @@
+package ru.pmishagin.Max;
+
+public class Max{
+
+ public int max(int first, int second; int result){
+
+ int first = 1;
+ int second = 2;
+ int result = second > first ? second : first;
+
+ }
+
+
+}
---
+++ chapter_001/src/test/java/ru/pmishagin/max/MaxTest.java
@@ -0,0 +1,19 @@
+package ru.pmishagin.max;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class MaxTest {
+
+    @Test
+
+    public void whenFirstlessSecond(){
+
+    	Max maxim = new Max();
+        int result = maxim.max(1, 2);
+        assertThat(result, is(2));
+
+    }
+}
