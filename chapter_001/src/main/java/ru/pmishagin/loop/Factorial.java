c72310e3ce048b03d11010c9ca6a08107688a0b9
Pavel71 <raz_3@mail.ru>
2017-10-25 10:24:43 +0300

    Factorial

---
+++ chapter_001/src/main/java/ru/pmishagin/loop/Factorial.java
@@ -0,0 +1,29 @@
+package ru.pmishagin.loop;
+
+
+
+
+public class Factorial {
+
+
+    public int calc (int n) {
+
+
+
+    int result=1;
+
+
+        for (int i = 1; i <= n; i++) {
+
+            result = result * i;
+        }
+
+
+        return result;
+
+
+    }
+
+
+
+}
---
+++ chapter_001/src/test/java/ru/pmishagin/loop/FactorialTest.java
@@ -0,0 +1,7 @@
+package ru.pmishagin.loop;
+
+/**
+ * Created by Home on 25.10.2017.
+ */
+public class FactorialTest {
+}
