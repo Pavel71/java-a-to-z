c3d32477b2a270a0756af94fb94144f7eb3082e8
Pavel71 <raz_3@mail.ru>
2017-10-31 13:43:36 +0300

    add array

---
+++ chapter_001/src/main/java/ru/pmishagin/array/Massif.java
@@ -0,0 +1,26 @@
+package ru.pmishagin.Array;
+
+import java.util.Arrays;
+
+/**
+ *  Massif class.
+ *
+ * @author Pavel Mishagin (mailto:raz_3@mail.ru)
+ * @version 0.1
+ * @since 26.10.2017
+ */
+public class Massif {
+
+    public static void main(String[] args) {
+
+        int[] mas = new int[]{1, 2, 3, 4, 5, 6, 7};
+
+        if(mas[mas.length-1]>mas[0]){
+
+            int c  = mas[0];
+            mas[0] = mas[mas.length-1];
+            mas[mas.length-1] = c;
+
+        }System.out.println(Arrays.toString(mas));
+    }
+}
