de082a1ef10603d91038774874feb0f216ae6f1d
Pavel71 <raz_3@mail.ru>
2017-10-31 13:42:35 +0300

    add array

---
+++ chapter_001/src/test/java/ru/pmishagin/array/BubleSortTest.java
@@ -0,0 +1,29 @@
+package ru.pmishagin.array;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+/**
+ * Test.
+ *
+ * @author Pavel Mishagin (mailto:raz_3@mail.ru)
+ * @version $1.0$
+ * @since 30.10.2017
+ */
+
+public class BubleSortTest {
+
+    @Test
+    public void BublesortIfmaxmin() {
+
+        BubleSort buble = new BubleSort();
+
+        int[] array = {5,2,8,12,9,7};
+
+        int[] result = buble.sort(array);
+
+        int[] expected = {2,5,7,8,9,12};
+
+        assertThat(result, is(expected));
+    }
+}
---
+++ chapter_001/src/test/java/ru/pmishagin/array/TurnTest.java
@@ -0,0 +1,28 @@
+package ru.pmishagin.array;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+/**
+ * Test.
+ *
+ * @author Pavel Mishagin (mailto:raz_3@mail.ru)
+ * @version $1.0$
+ * @since 30.10.2017
+ */
+public class TurnTest {
+
+    @Test
+    public void whenArrFromOneToTen() {
+
+        Turn turn = new Turn();
+
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        int[] result = turn.back(array);
+
+        int[] expected = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+
+        assertThat(result, is(expected));
+    }
+}
